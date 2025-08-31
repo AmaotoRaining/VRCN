0d898e5fe996d8669f7f123b584041d5b45dfda5
null_base <null.base.null@gmail.com>
2025-09-01 04:52:33 +0900

    ✨ ShareExtensionを追加し、Flutterエンジンを統合いたしましたわ。

---
+++ ios/ShareExtension/ShareViewController.swift
@@ -0,0 +1,24 @@
+import UIKit
+import Flutter
+
+class ShareViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        showFlutter()
+    }
+
+    func showFlutter() {
+        let flutterEngine = FlutterEngine(name: "my flutter engine")
+        flutterEngine.run()
+        let flutterViewController = FlutterViewController(engine: flutterEngine, nibName: nil, bundle: nil)
+        addChild(flutterViewController)
+        view.addSubview(flutterViewController.view)
+        flutterViewController.view.frame = view.bounds
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        self.extensionContext?.cancelRequest(
+            withError: NSError(domain: Bundle.main.bundleIdentifier!, code: 0))
+    }
+}
